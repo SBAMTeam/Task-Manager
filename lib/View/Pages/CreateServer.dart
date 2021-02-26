36f1bb68c6f0aefb06003da7e8212e0ae8426ab6
basharHR <beshrshehab999@gmail.com>
2021-02-26 20:33:47 +0200

    added validators. added regular expressions. attempt at fixing textfield errors.

--- lib/View/Pages/CreateServer.dart
+++ lib/View/Pages/CreateServer.dart
@@ -11,83 +11,122 @@ class CreateServer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    GlobalKey<FormState> _formKey = GlobalKey<FormState>();
     return Scaffold(
+      backgroundColor: Color(backgroundColor),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Container(
             padding: EdgeInsets.symmetric(
                 horizontal: Get.width / 8, vertical: Get.height / 35),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Center(
-                  child: Image.asset(
-                    'assets/images/CreateServerImage.png',
-                    scale: 2.8,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Image.asset(
+                      'assets/images/CreateServerImage.png',
+                      scale: 2.8,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: sizedBoxBigSpace,
-                ),
-                TextBuilder(
-                  text: 'Server Info:',
-                  fontSize: 41,
-                ),
-                SizedBox(
-                  height: sizedBoxSmallSpace,
-                ),
-                TextFieldBuilder(
-                  icon: Icons.dns,
-                  hint: 'Server Name',
-                ),
-                SizedBox(
-                  height: sizedBoxSmallSpace,
-                ),
-                TextFieldBuilder(
-                  hint: email,
-                  icon: Icons.email,
-                ),
-                SizedBox(
-                  height: sizedBoxSmallSpace,
-                ),
-                TextFieldBuilder(
-                  hint: 'Phone Number',
-                  icon: Icons.phone,
-                ),
-                SizedBox(
-                  height: sizedBoxSmallSpace,
-                ),
-                TextBuilder(
-                  text: 'SuperUser Info:',
-                  fontSize: 41,
-                ),
-                SizedBox(
-                  height: sizedBoxSmallSpace,
-                ),
-                TextFieldBuilder(
-                  icon: Icons.account_circle,
-                  hint: 'Username',
-                ),
-                SizedBox(
-                  height: sizedBoxSmallSpace,
-                ),
-                TextFieldBuilder(
-                  hint: password,
-                  icon: Icons.lock,
-                ),
-                SizedBox(
-                  height: sizedBoxSmallSpace,
-                ),
-                Center(
-                  child: ButtonBuilder(
-                    text: 'Create Server',
-                    onPress: () {
-                      Get.to(ServerCodeCreated());
+                  SizedBox(
+                    height: sizedBoxBigSpace,
+                  ),
+                  TextBuilder(
+                    text: 'Server Info:',
+                    fontSize: 41,
+                  ),
+                  SizedBox(
+                    height: sizedBoxSmallSpace,
+                  ),
+                  TextFieldBuilder(
+                    icon: Icons.dns,
+                    hint: 'Server Name',
+                    validatorFunction: (String value) {
+                      if (value.isEmpty) {
+                        return 'Server Name is required.';
+                      }
                     },
                   ),
-                )
-              ],
+                  SizedBox(
+                    height: sizedBoxSmallSpace,
+                  ),
+                  TextFieldBuilder(
+                    hint: email,
+                    icon: Icons.email,
+                    textInputType: TextInputType.emailAddress,
+                    validatorFunction: (String value) {
+                      if (value.isEmpty ||
+                          !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(value)) {
+                        return 'Please enter a valid $email.';
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: sizedBoxSmallSpace,
+                  ),
+                  TextFieldBuilder(
+                    hint: '$phone (+963..)',
+                    icon: Icons.phone,
+                    textInputType: TextInputType.phone,
+                    validatorFunction: (String value) {
+                      if (value.isEmpty ||
+                          !RegExp(r'^\+[1-9]{1}[0-9]{3,14}$')
+                              .hasMatch(value.trim())) {
+                        return 'Please enter a valid $phone with correct country code.';
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: sizedBoxSmallSpace,
+                  ),
+                  TextBuilder(
+                    text: 'SuperUser Info:',
+                    fontSize: 41,
+                  ),
+                  SizedBox(
+                    height: sizedBoxSmallSpace,
+                  ),
+                  TextFieldBuilder(
+                    icon: Icons.account_circle,
+                    hint: 'Username',
+                  ),
+                  SizedBox(
+                    height: sizedBoxSmallSpace,
+                  ),
+                  TextFieldBuilder(
+                    hint: password,
+                    icon: Icons.lock,
+                    obscure: true,
+                    textInputType: TextInputType.visiblePassword,
+                    validatorFunction: (String value) {
+                      if (value.isEmpty ||
+                          !RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})')
+                              .hasMatch(value.trim())) {
+                        return 'Password must contain uppercase and smallcase letters,\n special characters, and numbers.';
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: sizedBoxSmallSpace,
+                  ),
+                  Center(
+                    child: ButtonBuilder(
+                      text: 'Create Server',
+                      onPress: () {
+                        if (!_formKey.currentState.validate()) {
+                          return;
+                        }
+                        _formKey.currentState.save();
+                        Get.to(() => ServerCodeCreated());
+                      },
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
--- lib/View/Pages/Login.dart
+++ lib/View/Pages/Login.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:taskmanager/View/Components/ButtonBuiler.dart';
 import 'package:taskmanager/View/Components/TextBuilder.dart';
 import 'package:taskmanager/View/Components/TextFieldBuilder.dart';
 import 'package:taskmanager/View/Components/Constants.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 
 class Login extends StatefulWidget {
   Login({Key key}) : super(key: key);
@@ -14,103 +14,160 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  var _emailAddress = null,
+      _password = null,
+      _companyCode = null,
+      _enabled = true;
   @override
   Widget build(BuildContext context) {
+    GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
             padding: EdgeInsets.symmetric(
                 horizontal: Get.width / 8, vertical: Get.height / 13),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset('assets/images/signin_image.png'),
-                SizedBox(
-                  height: Get.height / 30,
-                ),
-                Row(
-                  children: [
-                    TextBuilder(text: 'Sign In:'),
-                  ],
-                ),
-                SizedBox(
-                  height: Get.height / 30,
-                ),
-                TextFieldBuilder(
-                  icon: Icons.email,
-                  hint: 'Email Address',
-                ),
-                SizedBox(
-                  height: Get.height / 60,
-                ),
-                TextFieldBuilder(
-                  icon: Icons.lock,
-                  hint: 'Password',
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    FlatButton(
-                      onPressed: () {},
-                      child: Text(
-                        'Forgot Password?',
-                        style: TextStyle(
-                          color: Color(
-                            textColor,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/signin_image.png'),
+                  SizedBox(
+                    height: Get.height / 30,
+                  ),
+                  Row(
+                    children: [
+                      TextBuilder(text: 'Sign In:'),
+                    ],
+                  ),
+                  SizedBox(
+                    height: Get.height / 30,
+                  ),
+                  TextFieldBuilder(
+                    hint: email,
+                    icon: Icons.email,
+                    textInputType: TextInputType.emailAddress,
+                    validatorFunction: (String value) {
+                      if (value.isEmpty ||
+                          !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(value)) {
+                        return 'Please enter a valid $email.';
+                      }
+                    },
+                  ),
+                  SizedBox(
+                    height: Get.height / 60,
+                  ),
+                  TextFieldBuilder(
+                    hint: password,
+                    icon: Icons.lock,
+                    obscure: true,
+                    textInputType: TextInputType.visiblePassword,
+                    validatorFunction: (String value) {
+                      if (value.isEmpty ||
+                          !RegExp(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})')
+                              .hasMatch(value.trim())) {
+                        return 'Password must contain uppercase and smallcase letters,\n special characters, and numbers.';
+                      }
+                    },
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      FlatButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Color(
+                              textColor,
+                            ),
                           ),
                         ),
                       ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: Get.height / 60,
+                  ),
+                  Text(
+                    'Enter code provided by your company',
+                    style: TextStyle(color: Color(textColor), fontSize: 18),
+                  ),
+                  SizedBox(
+                    height: Get.height / 60,
+                  ),
+                  PinCodeTextField(
+                    enabled: _enabled,
+                    validator: (value) {
+                      if (value.length < 6) {
+                        return 'Please enter a valid code';
+                      } else
+                        return null;
+                    },
+                    onSaved: (value) {
+                      _companyCode = value;
+                      print(_companyCode);
+                    },
+                    boxShadows: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.15),
+                          blurRadius: 4,
+                          offset: Offset(1, 3))
+                    ],
+                    enableActiveFill: true,
+                    length: 6,
+                    backgroundColor: Colors.black.withOpacity(0),
+                    obscureText: false,
+                    textStyle: TextStyle(
+                      color: Color(0xff3F3D56),
                     ),
-                  ],
-                ),
-                SizedBox(
-                  height: Get.height / 60,
-                ),
-                Text(
-                  'Enter code provided by your company',
-                  style: TextStyle(color: Color(textColor), fontSize: 18),
-                ),
-                SizedBox(
-                  height: Get.height / 60,
-                ),
-                PinCodeTextField(
-                  boxShadows: [
-                    BoxShadow(
-                        color: Colors.black.withOpacity(0.15),
-                        blurRadius: 4,
-                        offset: Offset(1, 3))
-                  ],
-                  enableActiveFill: true,
-                  length: 6,
-                  backgroundColor: Colors.black.withOpacity(0),
-                  obscureText: false,
-                  textStyle: TextStyle(
-                    color: Color(0xff3F3D56),
+                    pinTheme: PinTheme(
+                      selectedFillColor: Colors.white,
+                      selectedColor: Color(0xffFF6366),
+                      activeFillColor: Colors.white,
+                      inactiveFillColor: Colors.white,
+                      inactiveColor: Colors.white,
+                      activeColor: Color(0xff3F3D56),
+                      shape: PinCodeFieldShape.box,
+                      borderRadius: BorderRadius.circular(90),
+                      fieldHeight: 50,
+                      fieldWidth: 42,
+                    ),
+                    appContext: context,
+                    onChanged: (String value) {
+                      if (value.length > 5) {
+                        _companyCode = value;
+                        setState(() {
+                          _enabled = false;
+                        });
+                      }
+                    },
                   ),
-                  pinTheme: PinTheme(
-                    selectedFillColor: Colors.white,
-                    selectedColor: Color(0xffFF6366),
-                    activeFillColor: Colors.white,
-                    inactiveFillColor: Colors.white,
-                    inactiveColor: Colors.white,
-                    activeColor: Color(0xff3F3D56),
-                    shape: PinCodeFieldShape.box,
-                    borderRadius: BorderRadius.circular(90),
-                    fieldHeight: 50,
-                    fieldWidth: 42,
+                  SizedBox(height: sizedBoxSmallSpace),
+                  ButtonBuilder(
+                    text: 'Clear All',
+                    onPress: () {
+                      setState(() {
+                        _enabled = true;
+                      });
+                    },
                   ),
-                  appContext: context,
-                  onChanged: (String value) {},
-                ),
-                SizedBox(
-                  height: Get.height / 60,
-                ),
-                ButtonBuilder(
-                  text: 'Login',
-                ),
-              ],
+                  SizedBox(height: sizedBoxSmallSpace),
+                  ButtonBuilder(
+                    text: 'Login',
+                    onPress: () {
+                      if (!_formKey.currentState.validate()) {
+                        return;
+                      }
+                      _formKey.currentState.save();
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
